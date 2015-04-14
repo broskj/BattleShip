b03ec36965ca49b211b75b1a130e249b934aac8d
broskj <kjbrost@gmail.com>
2015-04-14 15:44:59 -0400

    initial commit

---
+++ Computer.h
@@ -0,0 +1,200 @@
+#ifndef COMPUTER_H_
+#define COMPUTER_H_
+
+#include "Definitions.h"
+
+class Computer : public Player
+{
+public:
+    int **ships;
+    std::vector<int> rowPattern;
+    std::vector<int> colPattern;
+    std::queue<int> nextRows;
+    std::queue<int> nextCols;
+    std::vector<int> prevRows;
+    std::vector<int> prevCols;
+    std::string *names;
+    bool *isSunk;
+    int **rows;
+    int ** cols;
+    int nextRowGuess;
+    int nextColGuess;
+    int difficulty;
+    bool lastWasHit;
+
+    Computer(int mode)
+    {
+        lastWasHit = false;
+        srand((time(NULL)));
+        difficulty = mode;
+        ships = new int *[MAX_SHIPS];
+        rows = new int *[MAX_SHIPS];
+        cols = new int *[MAX_SHIPS];
+        names = new std::string[MAX_SHIPS];
+        isSunk = new bool [MAX_SHIPS];
+
+        names[0] = std::string("Destroyer"); /* size 2 */
+        names[1] = std::string("Submarine"); /* size 3 */
+        names[2] = std::string("Frigate"); /* size 3 */
+        names[3] = std::string("Battleship"); /* size 4*/
+        names[4] = std::string("Carrier"); /* size 5*/
+
+        ships[0] = new int[2]; /* destroyer */
+        ships[1] = new int[3]; /* submarine */
+        ships[2] = new int[3]; /* frigate */
+        ships[3] = new int[4]; /* battleship */
+        ships[4] = new int[5]; /* carrier */
+
+        rows[0] = new int[2]; /* destroyer */
+        rows[1] = new int[3]; /* submarine */
+        rows[2] = new int[3]; /* frigate */
+        rows[3] = new int[4]; /* battleship */
+        rows[4] = new int[5]; /* carrier */
+
+        cols[0] = new int[2]; /* destroyer */
+        cols[1] = new int[3]; /* submarine */
+        cols[2] = new int[3]; /* frigate */
+        cols[3] = new int[4]; /* battleship */
+        cols[4] = new int[5]; /* carrier */
+
+        for(int i = 0; i < MAX_SHIPS; i++)
+            isSunk[i] = false;
+
+        if(difficulty == 0)
+        {
+            nextRowGuess = rand() % 10;
+            nextColGuess = rand() % 10;
+        }
+        else if(difficulty == 1) // hard
+        {
+            for(int i = 9; i >= 0; i--)
+            {
+                rowPattern.push_back(i);
+                colPattern.push_back(9-i);
+            }
+            for(int i = 9; i >= 0; i--)
+            {
+                rowPattern.push_back(i);
+                colPattern.push_back(i);
+            }
+
+            nextRowGuess = rowPattern.back();
+            nextColGuess = colPattern.back();
+        }
+        else
+            std::cout << "Something happened Computer.h" << std::endl;
+    }//end Computer
+
+    int getShipSize(int index)
+    {
+        switch(index)
+        {
+        case 0:
+            return 2;
+        case 1:
+            return 3;
+        case 2:
+            return 3;
+        case 3:
+            return 4;
+        case 4:
+            return 5;
+        default:
+            return -1;
+        }
+    }//end getShipSize
+
+    std::string getShipName(int index)
+    {
+        //returns name of ship at ships[index]
+        return names[index];
+    }
+
+    bool sunk(int index, int row, int col)
+    {
+        /*
+         * the sunk method is only called when a ship is hit.  therefore,
+         *  we need to change the value of ships to indicate it was hit.
+         */
+
+        for(int i = 0; i < MAX_SHIPS; i++)
+        {
+            if(row == rows[index][i] && col == cols[index][i])
+            {
+                /* indicates recent hit was on the [index]th ship */
+                ships[index][i] = 3; /* declare the hit at that spot*/
+                if(!isSunk[index])
+                {
+                    for(int i = 0; i < getShipSize(index); i++)
+                    {
+                        if(ships[index][i] != 3)
+                            return false;
+                    }
+                    isSunk[index] = true;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }//end sunk
+
+    void findNextCoordinates(void)
+    {
+        if(difficulty == 1)
+        {
+            nextRowGuess = rowPattern.back();
+            nextColGuess = colPattern.back();
+        } else
+        {
+            nextRowGuess = rand() % 10;
+            nextColGuess = rand() % 10;
+        }
+        if (nextRows.size() == 0)
+        {
+            rowPattern.pop_back();
+            colPattern.pop_back();
+            return;
+        }
+
+        int s = 0;
+        if (nextRows.size() < 14)   /* In East, skip South */
+        {
+            /* Removes all of East */
+            s = nextRows.front();
+            while (s != -1)
+            {
+                nextRows.pop();
+                nextCols.pop();
+                s = nextRows.front();
+            }
+            nextRows.pop();
+            nextCols.pop();
+        }
+        else if (nextRows.size() < 19)     /* At least one hit north so remove North and East */
+        {
+            /* Removes all of North */
+            s = nextRows.front();
+            while (s != -1)
+            {
+                nextRows.pop();
+                nextCols.pop();
+                s = nextRows.front();
+            }
+            nextRows.pop();
+            nextCols.pop();
+        } /* Otherwise, try East and remove just North */
+        s = nextRows.front();
+        while (s != -1)
+        {
+            nextRows.pop();
+            nextCols.pop();
+            s = nextRows.front();
+        }
+        nextRows.pop();
+        nextCols.pop();
+        return;
+    }//end findNextCoordinates
+
+};//end class Computer
+
+#endif
---
+++ Methods.h
@@ -0,0 +1,456 @@
+#ifndef METHODS_H_
+#define METHODS_H_
+
+#include <iostream>
+#include <cmath>
+#include <stdlib.h>
+#include "Player.h"
+#include "Board.h"
+#include "Definitions.h"
+using namespace std;
+
+/*
+TODO:
+*/
+
+bool rowWithinBounds(int row)
+{
+    if(row < 0 || row > 9)
+        return false;
+    return true;
+}//end rowWithinBounds
+
+bool colWithinBounds(char col)
+{
+    if(col < 'A' || col > 'J')
+        return false;
+    return true;
+}//end colWithinBounds
+
+bool isValid(int startRow, int endRow, char startColumn, char endColumn, Player *playerShip, Board *playerBoard, int shipSize)
+{
+    for(int r = startRow; r <= endRow; r++)
+    {
+        for(int c = convertColumn(startColumn); c <= convertColumn(endColumn); c++)
+        {
+            if(playerBoard->getIndex(r, c) != 0 )
+            {
+                //indicates overlapping ships.
+                //The board is reprinted, so that the player can see where they could alternatively place their ships
+                cout << "Invalid ship placement, cannot overlap ships." << endl;
+                return false;
+            }
+        }
+    }
+
+    if(startRow != endRow && startColumn != endColumn )
+    {
+        //indicates diagonal placement
+        //cls();
+        //playerBoard->printBoard(1);
+        //The board is reprinted, so that the player can see where they could alternatively place their ships
+
+        cout << "Invalid ship placement, cannot place diagonally." << endl;
+        //pause();
+        return false;
+    }
+
+    if(abs((float)(startRow - endRow))+1 != shipSize &&
+            abs((float)(convertColumn(startColumn) - convertColumn(endColumn)))+1 !=
+            shipSize)
+    {
+
+        //indicates an invalid ship size
+
+        //cls();
+        //playerBoard->printBoard(1);
+        //The board is reprinted, so that the player can see where they could alternatively place their ships
+
+        cout << "Invalid ship placement, ship must be of size " << shipSize << "." << endl;
+        //pause();
+        return false;
+    }
+    return true;
+}//end isValid
+
+void initialShipPlacement(Board *&playerBoard, Player *&playerShip)
+{
+    //This method is used to do the initial ship placement for each player, at the start of the game
+    //As parameters, it takes in a board object and a player object, corresponding to the same player
+
+    int startRow = 0;
+    int endRow = 0;
+    char startColumn = 'A';
+    char endColumn = 'A';
+    bool flag = false;
+
+    for(int i = 0; i < MAX_SHIPS; i++)
+    {
+        //a for loop is used to iterate through the number of ships that needs to be placed
+
+        do
+        {
+            if(!flag)
+            {
+                if(i != 0)
+                    cls();
+                playerBoard->printBoard(1);
+                //The player's board is printed, so they can see where they would like to place their ships
+            }
+
+            //Each player is prompted to enter the starting coordinate of the ship
+            cout << "Enter starting coordinate for ship of size " << playerShip->getShipSize(i) << " " << "\n";
+            cin >> startRow;
+
+            //This loop is used to continually ask the user for a correct input value, if an incorrect value is entered.
+            while (!rowWithinBounds(startRow))
+            {
+                cout << "Invalid entry, row must be between 0 and 9." << endl;
+                cin >> startRow;
+            }
+
+            //The char that they entered is converted to an uppercase letter, in case they entered a lowercase letter.
+            cin >> startColumn;
+            startColumn = toupper(startColumn);
+
+            //This loop is used to continually ask the user for a correct input value, if an incorrect value is entered.
+            while(!colWithinBounds(startColumn))
+            {
+                cout << "Invalid entry, column must be between A and J." << endl;
+                cin >> startColumn;
+                startColumn = toupper(startColumn);
+            }
+
+            //Then the player is prompted for the ending coordinate of the ship
+            cout << "Enter ending coordinate for ship of size " << playerShip->getShipSize(i) << " " << endl;
+            cin >> endRow;
+
+            //The loop checks and prompts the user until a valid input is given.
+            while(!rowWithinBounds(endRow))
+            {
+                cout << "Invalid entry, row must be between 0 and 9." << endl;
+                cin >> endRow;
+            }
+
+            //Finally, the user is prompted to enter the ending column of the ship
+            cin >> endColumn;
+
+            //The char that they entered is converted to an uppercase letter, in case they entered a lowercase letter.
+            endColumn = toupper(endColumn);
+
+            //Again, this loop is used to continually ask the user for a correct input value, if an incorrect value is entered.
+            while(!colWithinBounds(endColumn))
+            {
+                cout << "Invalid entry, column must be between A and J." << endl;
+                cin >> endColumn;
+                endColumn = toupper(endColumn);
+            }
+
+            //flag used to indicate incorrect ship size, position, etc.
+            flag = !isValid(startRow, endRow, startColumn, endColumn, playerShip, playerBoard, playerShip->getShipSize(i));
+
+        }
+        while(flag);
+
+        //once the user has sucessfully entered the columns and rows for their ships, the ship can officially be placed, using the place ship method.
+        playerBoard->placeShip(startRow, endRow, startColumn, endColumn, playerShip, i);
+
+    }
+}//end initialShipPlacement
+
+//This is a tuturial that shows the users how to play the game. It is an option on the menu
+void runTutorial()
+{
+
+    //Board objects are created for the tutorial
+    Board *board1 = new Board();
+    Board *board2 = new Board();
+
+    //Player objects are created for the tutorial
+    Player *computer1 = new Player;
+    Player *computer2 = new Player;
+
+    int startRow = 0;
+    int endRow = 0;
+    int fireRow = 0;
+    char startCol = 'A';
+    char endCol = 'A';
+    char fireCol = 'A';
+
+    //The tutorial begins to walk the player step by step on how to play the game
+    cout << "Welcome to the tutorial!" << endl;
+    cout << "First, let's begin by placing your ships." << endl << endl;
+    pause();
+    cls();
+
+    //prints the board for the user to see.
+    board1->printBoard(1);
+
+    //The user is prompted to enter the starting row to place a ship in a predetermined location
+    cout << "Our first ship is of size 2.  You'll need to first enter your ship's\n" <<
+         "starting coordinate.\nEnter '7B'."<< endl;
+
+    cin >> startRow;
+
+    //until the user enters these predetermined coordinates correctly, the loop conntinues
+    while(startRow != 7)
+    {
+        cout << "That's not the right row, enter '7'." << endl;
+        cin >> startRow;
+    }
+
+    //The user is prompted to enter the starting column to place a ship in a predetermined location
+    //cout << "Enter 'B' for the column." << endl;
+    cin >> startCol;
+
+    startCol = toupper(startCol);
+
+    //until the user enters these predetermined coordinates correctly, the loop conntinues
+    while(startCol != 'B')
+    {
+        cout << "That's not the right column, enter 'B'." << endl;
+        cin >> startCol;
+        startCol = toupper(startCol);
+    }
+
+    cls();
+    board1->printBoard(1);
+
+    //The user is prompted to enter the ending row to place a ship in a predetermined location
+    cout << "Now you'll enter the ship's ending coordinate.\nEnter '8B'." << endl;
+    cin >> endRow;
+
+    //until the user enters these predetermined coordinates correctly, the loop conntinues
+    while(endRow != 8)
+    {
+        cout << "That's not the right row, enter '8'." << endl;
+        cin >> endRow;
+    }
+
+    //The user is prompted to enter the ending column to place a ship in a predetermined location
+    //cout << "Enter 'B' for the column." << endl;
+    cin >> endCol;
+
+    endCol = toupper(endCol);
+
+    //until the user enters these predetermined coordinates correctly, the loop conntinues
+    while(endCol != 'B')
+    {
+        cout << "That's not the right column, enter 'B'." << endl;
+        cin >> endCol;
+        endCol = toupper(endCol);
+    }
+
+    //once the user has entered the points successfully, the ship is added to this location
+    board1->placeShip(7, 8, 'B', 'B', computer1, 2);
+
+    cls();
+    board1->printBoard(1);
+    cout << "Nice, you've placed a ship.  The process for the others is the same,\n" <<
+         "we'll go ahead and place your remaining ships." << endl << endl;
+
+    //The tutorial demonstrates ship placement, by helping the user place one ship, and goes ahead and places the rest for them.
+    board1->placeShip(2, 2, 'D', 'F', computer1, 3);
+    board1->placeShip(0, 2, 'I', 'I', computer1, 3);
+    board1->placeShip(5, 5, 'B', 'E', computer1, 4);
+    board1->placeShip(8, 4, 'H', 'H', computer1, 5);
+
+    pause();
+    cls();
+    board1->printBoard(1);
+
+    cout << "Here is your full board.  Now it's time for your opponent to place\n" <<
+         "their ships.  We'll take care of that for you." << endl << endl;
+
+    //The opponent's ships are placed on the respective board object
+    board2->placeShip(8, 8, 'H', 'G', computer2, 2);
+    board2->placeShip(7, 5, 'B', 'B', computer2, 3);
+    board2->placeShip(1, 3, 'H', 'H', computer2, 3);
+    board2->placeShip(0, 0, 'D', 'G', computer2, 4);
+    board2->placeShip(3, 7, 'J', 'J', computer2, 5);
+
+    pause();
+    cls();
+
+    //Two boards are printed for the user. Their own board, and that with their enemy's ships abstracted.
+
+    cout << endl << "Enemy Board" << endl;
+    board2->printBoard(2);
+    cout << "Your Board" << endl;
+    board1->printBoard(1);
+
+    //A message is printed educating the user on the different boards.
+    cout << "Here you see your board on top and your opponent's board without " <<
+         "their ships.\nYour opponent sees a similar screen during their turn.  " <<
+         "Now, your goal is\nto sink the enemy ships.  Let's take a shot at" <<
+         " 2B." << endl << endl;
+
+    //This stage of the tutorial takes the user through how to enter points to guess a ship
+
+    cin >> fireRow;
+
+    while(fireRow != 2)
+    {
+        cout << "That's not the right row, enter '2'." << endl;
+        cin >> fireRow;
+    }
+
+    cin >> fireCol;
+    fireCol = toupper(fireCol);
+
+    while(fireCol != 'B')
+    {
+        cout << "That's not the right column, enter 'B'." << endl;
+        cin >> fireCol;
+        fireCol = toupper(fireCol);
+    }
+
+    board2->checkGuess(2, convertColumn('B'), computer2);
+    cout << endl;
+    pause();
+    cls();
+
+    //The guess was a miss, so now it's time for the opponent to take a shot
+
+    cout << endl << "Enemy Board" << endl;
+    board2->printBoard(2);
+    cout << "Your Board" << endl;
+    board1->printBoard(1);
+    cout << "That spot was a miss, oh well.  Now your opponent is going to fire at 0A." << endl << endl;
+
+    pause();
+    board1->checkGuess(0, convertColumn('A'), computer1);
+    cout << endl;
+    pause();
+    cls();
+
+    //The enemy then hits one of the player's ships
+
+    cout << endl << "Enemy Board" << endl;
+    board2->printBoard(2);
+    cout << "Your Board" << endl;
+    board1->printBoard(1);
+    cout << "Your ship was hit!  In the interest of time, we'll assume you've grasped\n" <<
+         "the general idea and fast forward a bit." << endl << endl;
+    pause();
+
+    for(int i = 1; i < 10; i++)
+    {
+        board1->checkGuess(i, i, computer1);
+        if(i < 6)
+            board1->checkGuess(9-i, i, computer1);
+    }
+
+    //2-length ship
+    board2->checkGuess(8, convertColumn('G'), computer2);
+    board2->checkGuess(8, convertColumn('H'), computer2);
+
+    //first 3-length ship
+    board2->checkGuess(5, convertColumn('B'), computer2);
+    board2->checkGuess(6, convertColumn('B'), computer2);
+    board2->checkGuess(7, convertColumn('B'), computer2);
+
+    //second 3-length ship
+    board2->checkGuess(1, convertColumn('H'), computer2);
+    board2->checkGuess(2, convertColumn('H'), computer2);
+    board2->checkGuess(3, convertColumn('H'), computer2);
+
+    //4-length ship
+    board2->checkGuess(0, convertColumn('D'), computer2);
+    board2->checkGuess(0, convertColumn('E'), computer2);
+    board2->checkGuess(0, convertColumn('F'), computer2);
+    board2->checkGuess(0, convertColumn('G'), computer2);
+
+    //5-length ship, minus one segment
+    board2->checkGuess(3, convertColumn('J'), computer2);
+    board2->checkGuess(4, convertColumn('J'), computer2);
+    board2->checkGuess(5, convertColumn('J'), computer2);
+    board2->checkGuess(6, convertColumn('J'), computer2);
+    //board2->checkGuess(8, convertColumn('G'), computer2);
+
+    //maybe add some random misses
+
+    cls();
+    cout << endl << "Enemy Board" << endl;
+    board2->printBoard(2);
+    cout << "Your Board" << endl;
+    board1->printBoard(1);
+
+    cout << "Okay!  The enemy now has one ship left.  Sink it by firing at 7J." << endl;
+
+    cin >> fireRow;
+
+    while(fireRow != 7)
+    {
+        cout << "That's not the right row, enter '7'." << endl;
+        cin >> fireRow;
+    }
+
+    cin >> fireCol;
+    fireCol = toupper(fireCol);
+
+    while(fireCol != 'J')
+    {
+        cout << "That's not the right column, enter 'J'." << endl;
+        cin >> fireCol;
+        fireCol = toupper(fireCol);
+    }
+
+    board2->checkGuess(7, convertColumn('J'), computer2);
+    cout << endl;
+    pause();
+
+    cls();
+    cout << endl << "Enemy Board" << endl;
+    board2->printBoard(2);
+    cout << "Your Board" << endl;
+    board1->printBoard(1);
+
+    cout << "Player 1 wins!" << endl;
+
+    //tutorial finished
+    cout << "You're now all set to play!  Returning to main menu." << endl << endl;
+    pause();
+    cls();
+
+}//end runTutorial
+
+void printInstructions()
+{
+    //This method prints the instructions for the user, from the menu
+
+    cout << "PVP Directions: " << endl;
+    cout << "Object of the game: be the first to sink all 5 of your opponent's ships." << endl << endl;
+    pause();
+    cls();
+
+    cout << "Prepare for Battle: " << endl;
+    cout << "You and your opponent sit across from each other and pass the laptop between\n moves." << endl;
+    cout << "Follow the directions to place each of your 5 ships on your ocean grid.\n";
+    cout << "Ships are placed by entering the column and row coordinates for both the bow\n and stern of each ship." << endl;
+    cout << "Ships may be placed in any horizontal or vertical position within the grid.\n They may not, however, be placed diagonally.\n";
+    cout << "Ships may not be placed so that they overlap the edge of the grid or another\n ship.\n";
+    cout << "Ship positions will remain constant throughout the gameplay.\n";
+    cout << "Now pass the computer to allow your opponent to place their ships.\n\n";
+    pause();
+    cls();
+
+    cout << "Gameplay:\n";
+    cout << "On your display will be both yours and your opponent's boards.\n";
+    cout << "Your board will display ship locations with an 'S'.  Open water is marked '~'.\n";
+    cout << "Your opponent's board will initially only display open water (~).\n";
+    cout << "On your turn, you will enter the row and column coordinates for a shot at your\n opponent's ships.\n";
+    cout << "If the shot hits one of their ships, your display for their board will record\n an 'X' where the hit took place.\n";
+    cout << "If the shot misses any ships and lands in open water, the board will record an\n 'O'.\n";
+    cout << "The computer is now passed to your opponent and they likewise try to hit your\n ships.\n";
+    pause();
+    cls();
+
+    cout << "Repeat this process of shooting and switching until either player sinks all of\n the other player's ships and wins.\n";
+    cout << "Returning to main menu." << endl;
+    pause();
+    cls();
+
+}//end printInstructions
+
+#endif
